8e6eebdab7643dda022e9c15ae0042e5449bda6e
amit <amitsr4@gmail.com>
2023-06-11 13:33:02 +0300

    server implemented

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import products from "../products";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
 import {
@@ -13,9 +12,21 @@ import {
   ListGroupItem,
 } from "react-bootstrap";
 
+import axios from "axios";
+
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
